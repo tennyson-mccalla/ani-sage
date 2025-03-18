c849a8a8855f21543c8cdb206291f2f6768ab950
Tennyson McCalla <tennyson.mccalla@gmail.com>
2025-03-18 11:21:17 -0500

    fix: add UI dependencies and remove old src directory

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,7 +1,14 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function corsHeaders() {
   return {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   };
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
